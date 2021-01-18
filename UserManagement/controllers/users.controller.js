28644702c46383e0311a3ec023f87ec4d99588a9
mokhaled3003 <mo.khaled3003@gmail.com>
2021-01-19 01:40:16 +0200

    add validations and routes

--- UserManagement/controllers/users.controller.js
+++ UserManagement/controllers/users.controller.js
@@ -2,8 +2,8 @@ let {
     UsersService
 } = require('../services/users.service')
 
-module.exports.ProductsController = class {
-    async register(req, res) {
+module.exports.UsersController = class {
+    static async register(req, res) {
 
         let data = await UsersService.register(req.body);
         if (data.code == 200) {
@@ -13,7 +13,7 @@ module.exports.ProductsController = class {
         }
     }
 
-    async login(req, res) {
+    static async login(req, res) {
 
         let data = await UsersService.login(req.body);
         if (data.code == 200) {
